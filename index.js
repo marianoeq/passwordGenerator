b5619c0cd90af96581f223b2e2c5b7ad00d79040
marian.eq <mariano.eq@gmail.com>
2022-05-10 10:50:54 +0200

    function to generate password re-formulated

--- index.js
+++ index.js
@@ -2,56 +2,32 @@ const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+=";
 
-
 const button = document.querySelector(".gen-pass");
 
-const input1 = document.querySelector(".first-password")
-const input2 = document.querySelector(".second-password")
-const input3 = document.querySelector(".third-password")
-const input4 = document.querySelector(".fourth-password")
+const input1 = document.querySelector(".first-password");
+const input2 = document.querySelector(".second-password");
+const input3 = document.querySelector(".third-password");
+const input4 = document.querySelector(".fourth-password");
+
+const password = function () {
+  return Array(4)
+    .fill(0)
+    .flatMap(() => [
+      letters[Math.floor(Math.random() * letters.length)],
+      numbers[Math.floor(Math.random() * numbers.length)],
+      symbols[Math.floor(Math.random() * symbols.length)],
+    ])
+    .join("");
+};
 
 button.addEventListener("click", (e) => {
- e.preventDefault();
+  e.preventDefault();
 
-  const password = Array(4).fill(0).flatMap(() => 
-  [
-      letters[Math.floor(Math.random() * letters.length)],
-      numbers[Math.floor(Math.random() * numbers.length)],
-      symbols[Math.floor(Math.random() * symbols.length)]
-    ]
-  ).join('');
-  const password2 = Array(4).fill(0).flatMap(() => 
-    [
-      letters[Math.floor(Math.random() * letters.length)],
-      numbers[Math.floor(Math.random() * numbers.length)],
-      symbols[Math.floor(Math.random() * symbols.length)]
-    ]
-  ).join('');
-  const password3 = Array(4).fill(0).flatMap(() => 
-    [
-      letters[Math.floor(Math.random() * letters.length)],
-      numbers[Math.floor(Math.random() * numbers.length)],
-      symbols[Math.floor(Math.random() * symbols.length)]
-    ]
-  ).join('');
-  const password4 = Array(4).fill(0).flatMap(() => 
-    [
-      letters[Math.floor(Math.random() * letters.length)],
-      numbers[Math.floor(Math.random() * numbers.length)],
-      symbols[Math.floor(Math.random() * symbols.length)]
-    ]
-  ).join('');
-
-    input1.value = password;        
-    input2.value = password2;        
-    input3.value = password3;        
-    input4.value = password4;        
-    
-})
-
-
-
-
+  input1.value = password();
+  input2.value = password();
+  input3.value = password();
+  input4.value = password();
+});
 
 // Create 3 different variables with all charactes possibles to create a passwords object✅
 // letters/symbols/numbers✅
@@ -63,5 +39,3 @@ button.addEventListener("click", (e) => {
 //Grab each of the resulting variables with the 4 characters and join them in the empty variable created in the function.✅
 //selectthe first input element with the .first-password class✅
 // display into the input already selected by the class mentioned above the first password generated ✅
-
-
